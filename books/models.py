307b386a6388db983a773b5234b3a6463bf0da43
FaRass898 <jdjjdhhhdh82@gmail.com>
2024-06-08 01:17:38 +0600

    list_and_detail

--- books/models.py
+++ books/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+
+    GENRE_CHOICES = (
+        ('ФЭНТЕЗИ','ФЭНТЕЗИ'),
+        ("ФАНТАСТИКА","ФАНТАСТИКА"),
+        ("БОЕВИКИ", "БОЕВИКИ"),
+        ("ДЕТЕКТИВ", "ДЕТЕКТИВ"),
+        ("ДРАМА", "ДРАМА"),
+        ("ХОРРОР", "ХОРРОР"),
+
+    )
+    name = models.CharField(max_length=100, verbose_name='Напишите название книги')
+    author_name = models.CharField(max_length=100, verbose_name='Напишите имя автора')
+
+    image = models.ImageField(upload_to='programming_blog/', verbose_name='Загрузите фото')
+    short_description = models.TextField(verbose_name='Напишите описание')
+    data_release = models.DateField(verbose_name='Укажите дату выхода книги')
+    genre = models.CharField(max_length=100, verbose_name='Выберите жанр',
+                             choices=GENRE_CHOICES)
+    summary = models.FileField(upload_to='summary/', verbose_name='Загрузите  краткое содержание')
+    wiki = models.URLField(verbose_name='Вставьте сыллку на википедию')
+    created_at = models.DateField(auto_now_add=True, verbose_name='')
+
+    class Meta:
+        verbose_name = 'Книгу'
+        verbose_name_plural = 'Книги'
--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('books_catalog', views.books_list_view),
+    path('books_catalog/<int:id>', views.books_detail_view),
+
     path('personal_info/', views.personal_info_view, name='personal_info'),
     path('hobby/', views.hobby_view, name='hobby'),
     path('current_time/', views.current_time_view, name='current_time'),
